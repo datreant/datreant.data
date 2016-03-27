faab8764cc1c354b4ae7910214dbdb2244d085dc
David Dotson <dotsdl@gmail.com>
2016-03-26 21:15:13 -0700

    Release 0.6.0.

--- setup.py
+++ setup.py
@@ -10,9 +10,21 @@ For a basic installation just type the command::
 from setuptools import setup, find_packages
 
 setup(name='datreant.data',
-      version='0.6.0-dev',
+      version='0.6.0',
+      description='convenient data storage and retrieval in HDF5 for Treants',
       author='David Dotson',
       author_email='dotsdl@gmail.com',
+      url = 'http://datreant.org/',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
       packages=find_packages('src'),
       namespace_packages=['datreant'],
       package_dir={'': 'src'},
